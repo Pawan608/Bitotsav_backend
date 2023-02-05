a1b2387055e4e54d399a3ad5142623e7ea073e75
Pawan608 <agarwalpawan6089@gmail.com>
2023-02-05 23:55:03 +0530

    indexhojayega

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    // origin: "https://www.bitotsav.in",
-    origin: "http://localhost:3001",
+    origin: "https://www.bitotsav.in",
+    // origin: "http://localhost:3001",
   })
 );
 app.use(helmet());
